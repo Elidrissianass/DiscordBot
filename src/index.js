15a161df33c495c65fe6926346c92937d1211f4f
Elidrissianass <anassvo00@gmail.com>
2021-02-28 23:50:29 +0100

    asd

--- src/index.js
+++ src/index.js
@@ -3,4 +3,17 @@ require('dotenv').config();
 
 const token = process.env.DISCORD_TOKEN;
 const client = new Client();
-client.destroy(token);
+const PREFIX = "!"
+client.login(token);
+client.on('message',(message)=>{
+    const channel = message.channel.name;
+    if(channel === 'bot-channel' && !message.content.startsWith(PREFIX)){
+        message.delete();
+        message.reply('didnt i tell you about not nagging around, just vibe in to the music or I BONK YOU !!').then(sentMessage => sentMessage.delete({ timeout: 10000 }));
+    }
+    if(channel !== 'bot-channel' && message.content.startsWith(PREFIX))
+    {
+        message.delete();
+        message.reply('dont anger ur Daddy now, go to the bot channel !!').then(sentMessage => sentMessage.delete({ timeout: 10000 }));
+    }
+})
